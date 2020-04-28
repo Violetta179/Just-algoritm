8fd91b35e06c8dc69b280bbfc6d0d20c7bf457f7
Violetta179 <64166175+Violetta179@users.noreply.github.com>
2020-04-28 19:54:19 +0300

    Добавьте файлы проекта.

---
+++ Just Algoritm/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Just_Algoritm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Кнопка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Minimum";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(331, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 24);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Maximum";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(556, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(167, 22);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "По возрастанию";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(556, 104);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(176, 24);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "По убыванию";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(171, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(171, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(331, 73);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(191, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(331, 134);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(182, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(556, 64);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(167, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(556, 145);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(176, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Введите значения,затем нажмите кнопку";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Just Algoritm/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Just_Algoritm
+{
+    public partial class Form1 : Form
+    {
+        int[] mass = new int[10];
+        int q = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mass[q] = Convert.ToInt32(textBox1.Text);
+            q++;
+            textBox1.Text = " ";
+            string text = Convert.ToString(q);
+            if (q < 10)
+            {
+                textBox2.Text = "Введите " + text + "/9";
+            }
+            else
+            {
+                textBox2.Text = "Готово";
+                button1.Enabled = false;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int max = mass[0];
+            for (int i = 1; i < 9; i++)
+            {
+                if (max > mass[i])
+                {
+                    max = mass[i];
+                }
+            }
+            textBox3.Text = Convert.ToString(max);
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int min = mass[0];
+            for (int i = 1; i < 9; i++)
+            {
+                if (min < mass[i])
+                {
+                    min = mass[i];
+                }
+            }
+            textBox4.Text = Convert.ToString(min);
+            button3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int temp;
+
+            for (int i = 0; i < mass.Length - 1; i++)
+            {
+                for (int j = i + 1; j < mass.Length; j++)
+                {
+                    if (mass[i] > mass[j])
+                    {
+                        temp = mass[i];
+                        mass[i] = mass[j];
+                        mass[j] = temp;
+
+                    }
+                }
+            }
+            string result = "";
+            for (int i = 0; i < 10; i++)
+            {
+                result = result + Convert.ToString(mass[i]) + "  ";
+            }
+            textBox5.Text = result;
+            button4.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int temp;
+
+            for (int i = 0; i < mass.Length - 1; i++)
+            {
+                for (int j = i + 1; j < mass.Length; j++)
+                {
+                    if (mass[i] < mass[j])
+                    {
+                        temp = mass[i];
+                        mass[i] = mass[j];
+                        mass[j] = temp;
+
+                    }
+                }
+            }
+            string result = "";
+            for (int i = 0; i < 10; i++)
+            {
+                result = result + Convert.ToString(mass[i]) + "  ";
+            }
+            textBox6.Text = result;
+            button5.Enabled = false;
+        }
+    }
+}
